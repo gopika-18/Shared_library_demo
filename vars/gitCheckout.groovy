fff5b2de947aa131c43f943e5bd104c0360b377f
gopika-18 <95238412+gopika-18@users.noreply.github.com>
2022-02-19 16:44:05 +0530

    Update and rename gitCheckout.groovy to sharedlib.groovy

--- vars/gitCheckout.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map stageParams)
-{
-checkout([
- $class: 'GitSCM',
- branches: [[name: '*/main']], extensions: [], 
-userRemoteConfigs: [[credentialsId: 'github_login', url: 'https://github.com/gopika-18/Jenkins_exercise.git']]
-])
-}
---
+++ vars/sharedlib.groovy
@@ -0,0 +1,44 @@
+def call(String repoUrl) {
+pipeline {
+    agent any
+    tools {
+            maven 'Maven' 
+          }
+    stages {
+        stage('Clone sources') {
+            steps {
+                git branch: 'main',
+                url: "${repoUrl}"
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'mvn -V -U -e clean install -Dsurefire.useFile=false -Dmaven.test.skip=true'
+            } 
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test -U -Dsurefire.useFile=false -Dsurefire.useSystemClassLoader=false -DtestResources=src/test/java/org/knime/devops/exercise'
+            }
+        }
+        stage('Result') {
+            steps {
+                junit allowEmptyResults: true, checksName: '**/src/main/java/**', skipMarkingBuildUnstable: true, testResults: '**/target/surefire-reports/TEST-*.xml'
+            }
+        }
+        stage('SonarQube analysis') {
+            steps {
+                withSonarQubeEnv('SonarQube') {
+                    sh "mvn -V -U -e sonar:sonar"
+                }
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'target/*.jar', onlyIfSuccessful: true
+             build job: 'java_pipeline'
+        }
+    }   
+} 
+}
